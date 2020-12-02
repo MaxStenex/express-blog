05e042ffc4e923692d357eb1c0936c0a18cbae66
Maxim Rozinkevich <yourstenex@gmail.com>
2020-12-02 15:23:59 +0300

    added ts-node-dev instead of nodemon

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(cors());
+
+app.use("/", (req, res) => {
+  res.sendStatus(200);
+});
 
 app.listen(4000, () => console.log("Server running on port: 4000"));
