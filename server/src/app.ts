e2441bf0e49efb803807b0354f45d0e34cd013b6
Maxim <yourstenex@gmail.com>
2021-01-01 17:43:56 +0300

    static

--- server/src/app.ts
+++ server/src/app.ts
@@ -35,8 +35,10 @@ app.use("/auth/login", loginRouter);
 app.use("/posts", postsRouter);
 
 if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../client/build")));
+
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname + "../../client/build/index.html"));
+    res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
   });
 }
 
