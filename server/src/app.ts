b188f06e9233678e798dfa6c494e4ee9163b0a38
Maxim <yourstenex@gmail.com>
2021-01-01 17:37:28 +0300

    fixed static file resolve

--- server/src/app.ts
+++ server/src/app.ts
@@ -35,10 +35,8 @@ app.use("/auth/login", loginRouter);
 app.use("/posts", postsRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../../client/build"));
-
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../../client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname + "../../client/build/index.html"));
   });
 }
 
